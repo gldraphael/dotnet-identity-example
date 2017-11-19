975b846f88efc6750df0b6cc28bafb081467a452
Galdin Raphael <gldraphael@gmail.com>
2017-11-20 03:10:50 +0530

    Add the DbContext as a service

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -23,6 +24,9 @@ namespace Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+				options.UseMySQL(Configuration.GetConnectionString("database")), ServiceLifetime.Scoped);
+
             services.AddMvc();
         }
 
