9ce2f6cec9b6ec2ff63448f8907c70e357403e83
Galdin Raphael <gldraphael@gmail.com>
2017-11-20 19:00:40 +0530

    Add the DbContext constructors

--- Web/ApplicationDbContext.cs
+++ Web/ApplicationDbContext.cs
@@ -1,10 +1,20 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Web.Models;
 
 namespace Web
 {
     public class ApplicationDbContext  : IdentityDbContext<ApplicationUser>
     {
-        
+        public ApplicationDbContext()
+        {
+
+        }
+
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
     }
 }
