3742ab16c9a1b7a539828523b5979e8d17eca811
Galdin Raphael <gldraphael@gmail.com>
2017-11-21 16:26:34 +0530

    Add login and logout endpoints

---
+++ Web/Controllers/AuthController.cs
@@ -0,0 +1,61 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Web.Models;
+using Web.ViewModels.Auth;
+
+namespace Web.Controllers
+{
+    [Route("api/auth")]
+    public class AuthController : Controller
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AuthController(SignInManager<ApplicationUser> signInManager)
+		{
+			_signInManager = signInManager;
+		}
+
+        // POST: /auth/login
+		[HttpPost("login")]
+		public async Task<IActionResult> Login([FromBody]LoginViewModel vm)
+		{
+            // Validate the requests
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(); // TODO: Say what's wrong with the request
+			}
+
+			var result = await _signInManager.PasswordSignInAsync(
+                userName: vm.Username,
+                password: vm.Password,
+                isPersistent: true, // TODO: Get this from the viewmodel
+                lockoutOnFailure: true
+            );
+
+			if (result.RequiresTwoFactor)
+			{
+				return StatusCode(501);
+			}
+			if (result.IsLockedOut)
+			{
+				return StatusCode(423);
+			}
+			if (result.Succeeded)
+			{
+				return Ok();
+			}
+
+			return Unauthorized();
+		}
+
+		[Authorize, HttpPost("logout")]
+		public async Task<IActionResult> Logout()
+		{
+			await _signInManager.SignOutAsync();
+			return Ok();
+		}
+
+    }
+}
