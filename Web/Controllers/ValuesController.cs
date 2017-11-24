53b694d2bdb345834f24dd8f31dd96a0b6774d54
Galdin Raphael <gldraphael@gmail.com>
2017-11-25 00:13:57 +0530

    Only Authorize Role1 users to access /api/values*

--- Web/Controllers/ValuesController.cs
+++ Web/Controllers/ValuesController.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Web.Controllers
 {
+    [Authorize(Roles = "Role1")]
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
